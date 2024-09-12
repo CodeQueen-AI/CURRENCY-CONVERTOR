f39de32daefff191ba48d48bd645a39ead8cc508
Sumbal Naz <Codeq209@gmail.com>
2024-09-12 16:25:20 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency: any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: "Enter from Currency",
+            type:"list",
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name: 'to',
+            message: "Enter To Currency",
+            type:"list",
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:'amount',
+            message:'Enter Your Amount',
+            type:'number'
+        }
+    ]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount/ fromAmount
+let convertedamount = baseAmount * toAmount
+console.log(convertedamount)
+console.log(fromAmount)
+console.log(toAmount)
+console.log(amount)
